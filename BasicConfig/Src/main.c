fe3c2fba45dcc8c4c966a3ccf12dc9c287826cf8
Jemunozm <Jemunozm@unal.edu.co>
2023-09-19 09:56:00 -0500

    Creación BasicConfig
    Un proyecto con la configuración basica que se usará de ahora en adelante :D

---
+++ BasicConfig/Src/main.c
@@ -0,0 +1,33 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : ImJeviz
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+
+/**
+ * Funcion inicial del programa.
+ * Esta función es el corazón del programa
+ */
+int main(void)
+{
+    /* Loop forever */
+	while(1){
+
+	}
+
+	return 0;
+}
